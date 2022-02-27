b8a3acb26189a3dd2b244eada0f31eaaa4e797e0
p0lygun <solankivibhakar82@gmail.com>
2022-02-28 01:24:04 +0530

    pass middleware on Api init

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware import Middleware
 
 from .routes import auth
 
@@ -9,20 +10,24 @@ origins = [
     "http://localhost:8081",
     "http://localhost:8082",
 ]
+middleware = [
+    Middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["POST", "GET"],
+        allow_headers=["*"],
+        max_age=3600,
+    )
+]
+
 
 app = FastAPI(
     title="BFReport API docs",
     description="""BFReport is a shared report system for the Battlefield series\n
                 ...""",
     version="0.0.1",
-)
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["POST", "GET"],
-    allow_headers=["*"],
-    max_age=3600,
+    middleware=middleware
 )
 
 @app.get("/")
