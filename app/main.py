e50f2b532d3098338b627d5fe28986f5d5810ba2
Zefanja Jobse <zefanjaj@gmail.com>
2022-02-27 19:54:10 +0000

    not using add_middleware

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
+from fastapi.middleware import Middleware
 from fastapi.middleware.cors import CORSMiddleware
 
 from .routes import auth
@@ -9,33 +10,35 @@ app = FastAPI(
     description="""BFReport is a shared report system for the Battlefield series\n
                 ...""",
     version="0.0.1",
-)
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=[
-        "https://bfportal.com",
-        "http://localhost:8081",
-        "http://localhost:8082",
-    ],
-    allow_credentials=True,
-    allow_methods=["POST", "GET"],
-    allow_headers=[
-        "Access-Control-Allow-Headers",
-        "Origin",
-        "Accept",
-        "X-Requested-With",
-        "Content-Type",
-        "Access-Control-Request-Method",
-        "Access-Control-Request-Headers",
-        "Access-Control-Allow-Credentials",
-        "Access-Control-Allow-Origin",
-        "Access-Control-Allow-Methods"
-        "Authorization",
-        "X-Amz-Date",
-        "X-Api-Key",
-        "X-Amz-Security-Token"
-    ],
-    max_age=3600,
+    middleware = [
+        Middleware(
+            CORSMiddleware,
+            allow_origins=[
+            "https://bfportal.com",
+            "http://localhost:8081",
+            "http://localhost:8082",
+            ],
+            allow_credentials=True,
+            allow_methods=["POST", "GET"],
+            allow_headers=[
+                "Access-Control-Allow-Headers",
+                "Origin",
+                "Accept",
+                "X-Requested-With",
+                "Content-Type",
+                "Access-Control-Request-Method",
+                "Access-Control-Request-Headers",
+                "Access-Control-Allow-Credentials",
+                "Access-Control-Allow-Origin",
+                "Access-Control-Allow-Methods"
+                "Authorization",
+                "X-Amz-Date",
+                "X-Api-Key",
+                "X-Amz-Security-Token"
+            ],
+            max_age=3600,
+        )
+    ]
 )
 
 @app.get("/")
