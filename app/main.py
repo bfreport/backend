c20dbe36162c5d5b21af0ada8ed04a8dce6139b8
Zefanja Jobse <zefanjaj@gmail.com>
2022-02-27 20:27:50 +0000

    .

--- app/main.py
+++ app/main.py
@@ -3,40 +3,45 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from .routes import auth, main
 
-app = FastAPI(
-    title="BFReport API docs",
-    description="""BFReport is a shared report system for the Battlefield series\n
-                ...""",
-    version="0.0.1",
-)
+def create_app() -> CORSMiddleware:
+    """Create app wrapper to overcome middleware issues."""
+    fastapi_app = FastAPI(
+        title="BFReport API docs",
+        description="""BFReport is a shared report system for the Battlefield series\n
+                    ...""",
+        version="0.0.1",
+    )
+    
+    fastapi_app.include_router(main.router)
+    fastapi_app.include_router(auth.router)
+    
+    return CORSMiddleware(
+        fastapi_app,
+        allow_origins=[
+            "https://bfportal.com",
+            "http://localhost:8081",
+            "http://localhost:8082",
+        ],
+        allow_credentials=True,
+        allow_methods=["POST", "GET", "OPTIONS"],
+        allow_headers=[
+            "Access-Control-Allow-Headers",
+            "Origin",
+            "Accept",
+            "X-Requested-With",
+            "Content-Type",
+            "Access-Control-Request-Method",
+            "Access-Control-Request-Headers",
+            "Access-Control-Allow-Credentials",
+            "Access-Control-Allow-Origin",
+            "Access-Control-Allow-Methods"
+            "Authorization",
+            "X-Amz-Date",
+            "X-Api-Key",
+            "X-Amz-Security-Token"
+        ],
+        max_age=3600,
+    )
 
-app.include_router(main.router)
-app.include_router(auth.router)
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=[
-        "https://bfportal.com",
-        "http://localhost:8081",
-        "http://localhost:8082",
-    ],
-    allow_credentials=True,
-    allow_methods=["POST", "GET", "OPTIONS"],
-    allow_headers=[
-        "Access-Control-Allow-Headers",
-        "Origin",
-        "Accept",
-        "X-Requested-With",
-        "Content-Type",
-        "Access-Control-Request-Method",
-        "Access-Control-Request-Headers",
-        "Access-Control-Allow-Credentials",
-        "Access-Control-Allow-Origin",
-        "Access-Control-Allow-Methods"
-        "Authorization",
-        "X-Amz-Date",
-        "X-Api-Key",
-        "X-Amz-Security-Token"
-    ],
-    max_age=3600,
-)
+app = create_app()
